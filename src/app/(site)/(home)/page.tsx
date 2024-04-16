c2d74ca9470e1f7e4aecc05f7aebd10c3ac51a5b
theskinnycoder <18131a0505@gvpce.ac.in>
2024-04-17 04:07:53 +0530

    add revalidation logic

--- src/app/(site)/(home)/page.tsx
+++ src/app/(site)/(home)/page.tsx
@@ -4,6 +4,9 @@ import MainCarousel from "./_sections/main-carousel";
 import SlideFooter from "./_sections/slide-footer";
 import SlideHeader from "./_sections/slide-header";
 
+export const revalidate = 1000 * 60 * 60 * 24; // 1 day
+export const runtime = "edge";
+
 export default async function Home() {
 	const data = await sanityService.getAlbums();
 
