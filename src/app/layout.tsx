bc01260526200b6c392e17e692fcb6e8aba17247
theskinnycoder <18131a0505@gvpce.ac.in>
2024-06-22 02:02:56 +0530

    chore: Update font in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import { cn } from "@/lib/utils";
 import type { Metadata, Viewport } from "next";
-import { News_Cycle } from "next/font/google";
+import { Alegreya_Sans_SC } from "next/font/google";
 import { type PropsWithChildren } from "react";
 import "./globals.css";
 
-const font = News_Cycle({ subsets: ["latin"], weight: ["400", "700"] });
+const font = Alegreya_Sans_SC({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata: Metadata = {
 	title: "Prince VA",
